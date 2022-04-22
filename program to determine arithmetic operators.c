b6d48ca377dda2235f2b597a1eea4997c1c260da
margaretmumbi <102513019+margaretmumbi@users.noreply.github.com>
2022-04-22 14:48:36 +0300

    Add files via upload

---
+++ program to determine arithmetic operators.c
@@ -0,0 +1,19 @@
+// c program to determine arithmetic operators( +,-,*,/,%)
+#include<stdio.h>
+
+int main()
+{
+int a,b;
+a=5;
+b=3;
+float quotient;
+int product,sum,difference;
+sum=a+b;
+difference=a-b;
+product=a*b;
+quotient=a/b;
+return 0;
+}
+   
+
+    
